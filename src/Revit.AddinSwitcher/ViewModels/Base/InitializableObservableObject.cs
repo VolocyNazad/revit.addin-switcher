c8a4ab1e1ca9cceaca2a9fef1c560d12a87aa042
Korytin Nikita <korytin.nikita@gmail.com>
2025-09-19 08:46:42 +0500

    Создание базовой модели представления, для возможности ее инициализации с привязкой к xaml.

---
+++ src/Revit.AddinSwitcher/ViewModels/Base/InitializableObservableObject.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Revit.AddinSwitcher.ViewModels;
+
+internal abstract partial class InitializableObservableObject 
+    : ObservableObject
+{
+    private bool initialized;
+
+    #region [Initialize] Command - Инициализировать 
+
+    /// <summary> Инициализировать </summary>
+    [RelayCommand(CanExecute = nameof(CanInitialize))]
+    private void Initialize()
+    {
+        OnInitializing();
+        initialized = true;
+    }
+
+    private bool CanInitialize() => !initialized;
+
+    #endregion
+
+    #region [Deinitialize] Command - Деинициализировать 
+
+    /// <summary> Деинициализировать </summary>
+    [RelayCommand(CanExecute = nameof(CanDeinitialize))]
+    private void Deinitialize()
+    {
+        OnDeinitializing();
+        initialized = false;
+    }
+
+    private bool CanDeinitialize() => initialized;
+
+    #endregion
+
+    protected virtual void OnInitializing() { }
+    protected virtual void OnDeinitializing() { }
+}
