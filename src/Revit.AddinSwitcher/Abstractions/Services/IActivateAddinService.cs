1f9d12d93b476b657961546d9d2fdbe1085a98c3
Korytin Nikita <korytin.nikita@gmail.com>
2025-09-22 19:57:19 +0500

    Добавление сервиса для активации и деактивации плагинов.

---
+++ src/Revit.AddinSwitcher/Abstractions/Services/IActivateAddinService.cs
@@ -0,0 +1,8 @@
+﻿namespace Revit.AddinSwitcher.Abstractions.Services;
+
+internal interface IActivateAddinService
+{
+    Task Activate(string addinManifestPath);
+
+    Task Deactivate(string addinManifestPath);
+}
