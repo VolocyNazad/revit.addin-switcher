573c8c7c5d0bdb96b77764b94185eab6fb2cbac0
Korytin Nikita <korytin.nikita@gmail.com>
2025-09-22 19:58:49 +0500

    Добавление моделей представления: модель для визуализации информации и плагине, модель для визуализации представления о директории.

---
+++ src/Revit.AddinSwitcher/Abstractions/ViewModels/AddinInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Revit.AddinSwitcher.Abstractions.ViewModels;
+
+internal sealed record AddinInfoViewModel(string Path, bool IsActive)
+{
+    public string Name => System.IO.Path.GetFileNameWithoutExtension(Path) 
+        ?? string.Empty;
+    public string DirectoryPath => System.IO.Path.GetDirectoryName(Path) 
+        ?? string.Empty;
+    public string? Version => Directory.Exists(DirectoryPath) 
+        ? new DirectoryInfo(DirectoryPath).Name 
+        : string.Empty;
+}
